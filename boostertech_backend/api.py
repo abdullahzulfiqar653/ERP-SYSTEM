4600f3f224923389e636673a78b80059aaf3e3ab
abdullahzulfiqar0345 <abdullah@beyonderissolutions.com>
2022-02-17 14:09:58 +0500

    Initial changes Done
    Project setup complete accrding to lead(Mr Mohsin) and some important
    varables added to .env. So be sure to have .env in your project
    directory and paste variables from .env.example

--- boostertech_backend/api.py
+++ boostertech_backend/api.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 
 
 urlpatterns = [
-	# path('user/', include('user.api')),
+    path('auth/', include('rest_framework.urls')),
 ]
--- boostertechapp/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Test(models.Model):
-    test = models.CharField(max_length=10, default='')
