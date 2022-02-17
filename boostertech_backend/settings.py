59330598b5a515422e9e56f31b798f757171370b
shahroz akbar dhillon <shahroz.akbar@beyonderissolutions.com>
2022-02-17 17:06:56 +0500

    Update settings.py

--- boostertech_backend/settings.py
+++ boostertech_backend/settings.py
@@ -8,9 +8,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-
-print(BASE_DIR)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
